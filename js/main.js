bc0ca9466481e4fa1ae2fbff54d02b8faee5d343
rh64s <cchelovekov64@gmail.com>
2026-02-02 21:34:46 +0700

    practic 4 (theme 5)

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: false,
         inventory: 100,
-        onSale: true,
+        onSale: false,
+        details: ['80% cotton', '20% polyester', 'Gender-natural'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
+
     }
 })
