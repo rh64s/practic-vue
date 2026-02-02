d74d0fd023a487458669662cdaf10b47a036728a
rh64s <cchelovekov64@gmail.com>
2026-02-02 21:21:18 +0700

    practic 3 (theme 4)

--- js/main.js
+++ js/main.js
@@ -6,5 +6,8 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: false,
+        inventory: 100,
+        onSale: true,
     }
 })
