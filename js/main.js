ae239c0c6579a5bc7d194ba3c8ec75682479022d
rh64s <cchelovekov64@gmail.com>
2026-02-03 10:40:10 +0700

    practic 7

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let app = new Vue({
     el:"#app",
     data:{
         product: "Socks",
+        brand: "Vue Mastery",
         description: "A pair of warm, fuzzy socks",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
         inventory: 100,
         onSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-natural'],
@@ -15,16 +15,19 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
 
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
+        
     },
     methods: {
         addToCart() {
@@ -33,10 +36,24 @@ let app = new Vue({
         removeFromCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            return this.brand + ' ' + this.product + ' ' + (["not on sale", "on sale!"])[Number(this.onSale)];
         }
-
     }
 
 })
