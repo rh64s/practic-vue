181396aba293715176fb91f7e83ddfe385204cb5
rh64s <cchelovekov64@gmail.com>
2026-02-06 14:49:43 +0700

    add: columns

--- js/main.js
+++ js/main.js
@@ -1,3 +1,49 @@
+Vue.component('task', {
+})
+
+Vue.component('card', {
+    data: {
+        tasks: []
+    }
+})
+
+Vue.component('column', {
+    data: {
+        cards: [],
+    },
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        max: {
+            type: Number,
+            required: false,
+            default: 0
+        }
+    },
+    template: `
+    <div class="column">
+        <h3 class="column-title">{{ name }}</h3>
+        <div class="card" v-for="(card, cards)">
+            <card></card>
+        </div>
+        <
+    </div>
+    
+    `
+})
+
 let app = new Vue({
     el: '#app',
+    data: {
+        columns: []
+    },
+    template: `
+    <div class="todo-body">
+        <column :max="3" :name="'Первый столбец'"></column>
+        <column :max="5" :name="'Второй столбец'"></column>
+        <column :name="'Третий столбец'"></column>
+    </div>
+`
 })
