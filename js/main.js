87b75e03221f218ea0bfbc6998a64618b63fdb67
rh64s <cchelovekov64@gmail.com>
2026-02-07 22:15:02 +0700

    change column, create modal window for carts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,56 @@
 let eventBus = new Vue()
+
+Vue.component('createTask', {
+    data() {
+        return {
+            name: "",
+            tasks: [],
+            isDisabled: true
+        }
+    },
+    template: `
+<div>
+    <button type="submit" class="button-create" v-on:click="changeVisibility">Создать карточку</button>
+    <div class="modal" v-bind:class="{ isvisible: isDisabled }">
+        <div class="modal-background" v-on:click="changeVisibility"></div>
+        <form class="form-create-task" @submit.prevent="addCart">
+            <div class="form-group">
+                <label for="form-cart-name">Название</label>
+                <input id="form-cart-name" type="text" v-model="name">
+            </div>
+            <button type="button" class="button-second" v-on:click="addTask">Добавить задание</button>
+            <div class="form-group">
+                <div class="form-task" v-for="(task, index) in tasks">
+                    <label>Задание {{index}}</label>
+                    <input type="text" v-model="tasks[index].description">
+                </div>
+            </div>
+            
+            <button type="submit" class="button-create" v-on:click="changeVisibility">Создать карточку</button>
+        </form>
+    </div>
+</div>
+    `,
+    methods: {
+        addCart() {
+            console.log(this.name)
+            console.log(this.tasks.length)
+            this.$emit('add-cart', this.name, this.tasks);
+            this.name = ""
+            this.tasks = []
+        },
+        addTask() {
+            this.tasks.push({
+                "description": "пример слова",
+                "isChecked": false,
+            })
+        },
+        changeVisibility() {
+            this.isDisabled = !this.isDisabled
+        }
+    },
+})
+
 Vue.component('task', {
     props: {
         task: {
@@ -14,7 +66,7 @@ Vue.component('task', {
     <div class="task">
         <p>{{ task.description }}</p>
         <label>
-            <input type="checkbox" v-model="task.isChecked" v-on:change="sendTaskStatus">
+            <input type="checkbox" v-model="task.isChecked" v-on:click="sendTaskStatus">
         </label>
     </div>
     `,
@@ -44,7 +96,7 @@ Vue.component('card', {
     },
     template: `
         <div class="card">
-            <p>{{ this.name }}</p>
+            <p>{{ this.name }}, index = {{ this.index }}</p>
             <div class="card-task">
                 <task v-for="(task, index) in tasks" 
                         :task="task" :index="index"
@@ -54,7 +106,18 @@ Vue.component('card', {
     `,
     methods: {
         sendTaskStatus(task, taskIndex) {
-            eventBus.$emit('changeTaskStatus', this.index, task, taskIndex);
+            eventBus.$emit('changeTaskStatus', this.index, this.completedPercent);
+        }
+    },
+    computed: {
+        completedPercent() {
+            let completedPercent = 0;
+            for (task of this.tasks) {
+                if (task.isChecked) {
+                    completedPercent++;
+                }
+            }
+            return (completedPercent / this.tasks.length) * 100;
         }
     }
 })
@@ -79,7 +142,7 @@ Vue.component('column', {
     template: `
         <div class="column">
             <p class="column-title">{{ this.name }}</p>
-            <card v-for="(card, index) in cards" :index="index" :name="card.name" :tasks="card.tasks"></card>
+            <card v-for="card in cards" :index="card.id" :name="card.name" :tasks="card.tasks"></card>
         </div>
     `,
 });
@@ -87,6 +150,7 @@ Vue.component('column', {
 let app = new Vue({
     el: '#app',
     data: {
+        nextId: 0, // следующий id (index в template) внутри объекта card при добавлении
         columns: [
             {
                 max: 3,
@@ -101,54 +165,39 @@ let app = new Vue({
                 name: "Третий столбец",
             },
         ],
-        cards: [
-            {
-                name: "Card",
-                tasks: [
-                    {
-                        "description": "asdasd",
-                        "isChecked": false,
-                    },
-                    {
-                        "description": "asdasd",
-                        "isChecked": false,
-                    },
-                    {
-                        "description": "asdasd",
-                        "isChecked": false,
-                    },
-                    {
-                        "description": "asdasd",
-                        "isChecked": false,
-                    },
-                ],
-                columnNum: 0,
-            },
-        ],
+        cards: [],
     },
     methods: {
-        addCart() {
-            cards.push({
-                name: "Card",
-                tasks: [
-                    {
-                        "description": "asdasd",
-                        "isChecked": false,
-                    }
-                ],
+        addCart(name, tasks) {
+            console.log(name, tasks)
+            this.cards.push({
+                id: this.nextId++,
+                name: name,
+                tasks: tasks,
+                columnNum: 0,
             })
+        },
+        
+        checkCard(cardIndex, completedPercent) {
+            console.log(cardIndex, completedPercent);
+            this.cards[cardIndex].columnNum = Math.floor(completedPercent / 50);
         }
+        
     },
     computed: {
         cardsToColumn() {
             return this.columns.map((column, index) => {
-                return this.cards.filter(card => card.columnNum === index)
+                return this.cards.filter(function (card) {
+                    return card.columnNum === index;
+                })
             })
         }
     },
     mounted() {
-        eventBus.$on('changeTaskStatus', function (cardIndex, task, taskIndex) {
-            console.log("ага", task.isChecked, taskIndex);
-        })
-    }
+        this.cards = localStorage.getItem("cards") ? JSON.parse(localStorage.getItem("cards")) : [];
+        let checkCard = this.checkCard.bind(this);
+        eventBus.$on('changeTaskStatus', function (cardIndex, completedPercent) {
+            checkCard(cardIndex, completedPercent);
+        });
+    },
 })
