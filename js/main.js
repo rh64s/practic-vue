ff7812b13cead41055f1a9db2c1f4850ed5be7ec
rh64s <cchelovekov64@gmail.com>
2026-02-13 01:02:38 +0700

    feat: change card

--- js/main.js
+++ js/main.js
@@ -1,31 +1,86 @@
 let eventBus = new Vue()
 
 Vue.component('card', {
-    props: ['card'],
+    data() {
+        return {
+            currentModalMode: 0,
+            errors: []
+        }
+    },
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+    },
     template: `
 <div class="card">
-    <div class="card-header">
-        <p class="card-name">{{card.name}}</p>
-    </div>
-    <div class="card-info">
-        <p class="card-description">{{card.description}}</p>
-        <p class="card-created_at">Создана: {{new Date(card.created_at).toLocaleString('ru-RU')}}</p>
-        <p class="card-created_at" v-if="card.updated_at != null">Изменена: {{new Date(card.updated_at).toLocaleString('ru-RU')}}</p>
-        <p class="card-deadline">Дедлайн: {{new Date(card.deadline).toLocaleString('ru-RU')}}</p>
-    </div>
-    <div class="card-controller">
-        <div v-if="card.column_id < 4">
-            <button class="card-button-delete" v-on:click="deleteCard">Удалить</button>
+    <div v-if="currentModalMode === 0">
+        <div class="card-header">
+            <p class="card-name">{{ card.name }}</p>
         </div>
-        <div>
-            <button class="card-button-move" v-if="card.column_id > 1" v-on:click="moveCard(-1)"><-</button>
-            <button class="card-button-move" v-if="card.column_id < 4" v-on:click="moveCard(+1)">-></button>
+        <div class="card-info">
+            <p class="card-description">{{card.description}}</p>
+            <p class="card-created_at">Создана: {{new Date(card.created_at).toLocaleString('ru-RU')}}</p>
+            <p class="card-created_at" v-if="card.updated_at != null">Изменена: {{new Date(card.updated_at).toLocaleString('ru-RU')}}</p>
+            <p class="card-deadline">Дедлайн: {{new Date(card.deadline).toLocaleString('ru-RU')}}</p>
+        </div>
+        <div class="card-controller">
+            <div v-if="card.column_id < 4">
+                <button class="card-button-delete" v-on:click="deleteCard">Удалить</button>
+                <button class="card-button-change" v-on:click="currentModalMode = 1">Изменить</button>
+            </div>
+            <div>
+                <button class="card-button-move" v-if="card.column_id > 1" v-on:click="moveCard(-1)"><-</button>
+                <button class="card-button-move" v-if="card.column_id < 4" v-on:click="moveCard(+1)">-></button>
+            </div>
         </div>
     </div>
+    <div v-else-if="currentModalMode === 1">
+        <form @submit.prevent="saveCard">
+            <div class="form-group">
+                <label>Название задачи</label>
+                <input type="text" v-model="card.name" placeholder="Название">
+            </div>
+            <div class="form-group">
+                <label>Описание задачи</label>
+                <textarea v-model="card.description"></textarea>
+            </div>
+            <div class="form-group">
+                <label>Дэдлайн</label>
+                <input type="date" v-model="card.deadline">
+            </div>
+            <button type="submit" class="btn btn-primary">Сохранить задачу</button>
+            <button type="button" class="btn btn-secondary" v-on:click="currentModalMode = 0">Закрыть</button>
+        </form>
+        <div class="errors" v-if="errors.length > 0">
+            <p class="error" v-for="error in errors">{{error}}</p>
+        </div>
+    </div>
+</div>
 </div>`,
     methods: {
         moveCard(direction) { eventBus.$emit('move-card', this.card.id, direction); },
         deleteCard() {eventBus.$emit('delete-card', this.card.id); },
+        saveCard() {
+            this.errors = []
+            if (this.card.name === "") {
+                this.errors.push("Введите название задачи")
+            }
+            if (this.card.description === "") {
+                this.errors.push("Введите описание задачи")
+            }
+            if (this.card.deadline === null) {
+                this.errors.push("Укажите дэдлайн!")
+            } else if (new Date(this.card.deadline) < Date.now()) {
+                this.errors.push("Дэдлайн не может быть раньше, чем текущее время")
+            }
+            if(this.errors.length > 0) {
+                return;
+            }
+            this.currentModalMode = 0;
+            eventBus.$emit('save-card', this.card);
+        }
     }
 })
 
@@ -110,7 +165,7 @@ Vue.component('column', {
     <p class="column-title">{{ this.column.name }}</p>
     <div class="cards">
         <create-form v-if="index === 0" @create-card="createCard"></create-form>
-        <card v-for="card in cards" :key="card.id" :card="card"></card>
+        <card v-for="card in cards" :key="card.id" :card="card" ></card>
     </div>
 </div>`,
     methods: {
@@ -135,7 +190,7 @@ let app = new Vue({
     methods: {
         createCard(card) {
             this.cards.push({
-                id: this.cards.length,
+                id: typeof(this.cards[this.cards.length - 1]) != 'undefined' ? this.cards[this.cards.length - 1].id + 1 : 0,
                 name: card.name,
                 description: card.description,
                 deadline: card.deadline,
@@ -147,19 +202,26 @@ let app = new Vue({
             this.saveCards()
             this.cards = [...this.cards];
         },
+        getCard(cardId) {
+            return this.cards.find((card) => card.id === cardId);
+        },
         moveCard(cardId, direction) {
-            this.cards[cardId].column_id += direction;
+            this.getCard(cardId).column_id += direction;
             this.saveCards()
         },
         saveCards() {
             localStorage.setItem("cards", JSON.stringify(this.cards))
         },
+        saveCard(newCard) {
+            let card = this.getCard(newCard.id);
+            card.name = newCard.name;
+            card.description = newCard.description;
+            card.deadline = newCard.deadline;
+            card.updated_at = Date.now();
+            this.saveCards()
+        },
         deleteCard(cardId) {
-            this.cards.pop(cardId);
-            let index = 0;
-            this.cards.forEach((card) => {
-                card.id = index++;
-            })
+            this.cards = this.cards.filter(card => card.id !== cardId);
             this.saveCards()
         }
     },
@@ -176,5 +238,6 @@ let app = new Vue({
         this.cards = localStorage.getItem("cards") ? JSON.parse(localStorage.getItem("cards")) : [];
         eventBus.$on('move-card', this.moveCard);
         eventBus.$on('delete-card', this.deleteCard);
+        eventBus.$on('save-card', this.saveCard);
     }
 })
